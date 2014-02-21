7ae65b7f7f8f4b4d8526ab837d31620909d093a0
cippo1987 <f.brivio@bath.ac.uk>
2014-02-21 19:46:21 +0000

    Update function.sh

--- function.sh
+++ function.sh
@@ -1,20 +1,22 @@
-  1 setpar(){
-  2 
-  3 NSW=$1
-  4 ISIF=$2
-  5 
-  6 #sed -i '23i NSW = 0' INCAR
-  7 rm foo foo2
-  8 cp INCAR.o INCAR
-  9 sed '/NSW/d' INCAR >> foo
- 10 sed '/ISIF/d' foo >> foo2
- 11 #NSW='300'
- 12 #ISIF='2'
- 13 sed -i "23i NSW = "$NSW"" foo2
- 14 sed -i "26i ISIF = "$ISIF"" foo2
- 15 #mv foo2 INCAR2
- 16 }
- 17 #to call the function
- 18 setpar 123 456
- 
-~                          
+setpar(){
+        NSW=$1
+        ISIF=$2
+        rm foo foo2
+        cp INCAR.o INCAR
+        sed '/NSW/d' INCAR >> foo 
+        sed '/ISIF/d' foo >> foo2
+        sed -i "23i NSW = "$NSW"" foo2
+        sed -i "26i ISIF = "$ISIF"" foo2
+        mv foo2 INCAR
+}   
+
+runISIF2(){
+        setpar 300 2
+        COUNTER=$((COUNTER + 1)) 
+        cp INCAR INCAR_${COUNTER}
+         aprun -n $NPROC -N $NTASK /home/e05/e05/federico/bin/VASP/vasp5 > out${I}.out
+         E=`tail -n 2 OSZICAR | awk '{ print $3}'`
+         echo $COUNTER $ISIF $E >> Energylist.dat
+         cp CONTCAR CONTCAR_${I}
+         cp CONTCAR POSCAR
+}
